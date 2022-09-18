e82d50757d1962c401207117b1e48134a9d657c5
B6Infinity <broteendas.official@gmail.com>
2022-09-18 11:48:18 +0530

    lookin good eh

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            // entry.target.classList.remove('anim-hidden');
+            entry.target.classList.add('anim-show');
+        }else{
+            entry.target.classList.remove('anim-show');
+            // entry.target.classList.add('anim-hidden');
+        }
+    });
+});
+
+
+const hiddenElements = document.getElementsByClassName('anim-hidden'); // .anim-show
+// hiddenElements.forEach((el) => observer.observe(el));
+console.log(hiddenElements);
+for(let i = 0; i < hiddenElements.length; i++){
+    observer.observe(hiddenElements[i]);
+}
